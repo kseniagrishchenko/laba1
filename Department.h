90635a7cfeb436835c905ebc2c9d00443e5c0145
kseniagrishchenko <ksusha1514@mail.ru>
2016-12-15 10:58:43 +0300

    add department and main program (cpp, h)

--- Department.h
+++ Department.h
@@ -1,12 +1,14 @@
 #pragma once
+#include <memory>
+#include <vector>
 #include "MailReceiver.h"
 #include "Mediator.h"
 
-class CUser: public CMailReceiver
+class CDepartment: public CMailReceiver
 {
 public:
-	CUser(void);
-	~CUser(void);
+	CDepartment(void);
+	~CDepartment(void);
 
 	// CColleague interface
 	void add_mediator(CMediator* mediator);
@@ -15,10 +17,14 @@ public:
 	// CMailReceiver interface
 	void receive_mail(const std::string& message, const std::string& target_name);
 	void set_name(const std::string& name);
-	std::string get_name(void);
-private:
-	std::string m_user_name;
-	CMediator* m_mail_mediator;
-};
+	std::string get_name();
 
+	// Composite interface
+	void add_mail_receiver(CMailReceiver* receiver);
+	void remove_mail_receiver(CMailReceiver* receiver);
+private:
+	std::string m_department_name;
+	CMediator* m_mail_mediator;
+	std::vector<CMailReceiver*> m_mail_receivers;
+};
 
